2822b1f9b84c15ae1dbdfe5d7b21991cd9134179
Annamalai <annamalai.chockalingam@gmail.com>
2015-02-26 02:15:10 +0530

    1st Commit towards maven-tycho examples

---
+++ org.ancit.examples.application/src/org/ancit/examples/application/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package org.ancit.examples.application;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "org.ancit.examples.application.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
